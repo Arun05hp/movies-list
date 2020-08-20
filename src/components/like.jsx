95d1d284206e77addc3085ea3aafbfe5cd584231
Arun Kumar <kumar05.962013@gmail.com>
2020-08-20 16:40:39 +0530

    like.jsx changing in stateless function

--- src/components/like.jsx
+++ src/components/like.jsx
@@ -1,21 +1,16 @@
-import React, { Component } from "react";
-class Like extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
-    return (
-      <i
-        onClick={this.props.onClick}
-        style={{ cursor: "pointer" }}
-        className={classes}
-        aria-hidden="true"
-      ></i>
-    );
-  }
-}
+import React from "react";
+
+const Like = ({ liked, onClick }) => {
+  let classes = "fa fa-heart";
+  if (!liked) classes += "-o";
+  return (
+    <i
+      onClick={onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
 
 export default Like;
