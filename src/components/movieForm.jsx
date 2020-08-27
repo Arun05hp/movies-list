acb745d519c409fc3a776eb9102a94177f8b292a
Arun Kumar <kumar05.962013@gmail.com>
2020-08-27 11:05:47 +0530

    change fakeServices in movie form

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Form from "./common/form";
 import Joi from "joi-browser";
-import { getGenres } from "../services/fakeGenreService";
-import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/genreService";
+import { getMovie, saveMovie } from "../services/movieService";
 
 class MovieForm extends Form {
   state = {
@@ -22,13 +22,14 @@ class MovieForm extends Form {
       .label("Number in Stock"),
     dailyRentalRate: Joi.number().min(0).max(10).label("Daily Rental Rate"),
   };
-  componentDidMount() {
-    const genres = getGenres();
+  async componentDidMount() {
+    const { data: genres } = await getGenres();
+
     this.setState({ genres });
 
     const movieId = this.props.match.params.id;
     if (movieId === "new") return;
-    const movie = getMovie(movieId);
+    const { data: movie } = await getMovie(movieId);
     if (!movie) return this.props.history.replace("/not-found");
     this.setState({ data: this.mapToViewModal(movie) });
   }
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,10 +1,31 @@
 import http from "./httpService";
+import { getGenres } from "./genreService";
 import { apiUrl } from "../config.json";
 
 const apiEndPoint = apiUrl + "/movies";
 
 export function getMovies() {
   return http.get(apiEndPoint);
+}
+
+export function getMovie(id) {
+  return http.get(apiEndPoint + "/" + id);
+}
+export async function saveMovie(movie) {
+  const { data: movies } = await getMovie();
+  let movieInDb = movies.find((m) => m._id === movie._id) || {};
+  movieInDb.title = movie.title;
+  const genres = await getGenres();
+  movieInDb.genre = genres.find((g) => g._id === movie.genreId);
+  movieInDb.numberInStock = movie.numberInStock;
+  movieInDb.dailyRentalRate = movie.dailyRentalRate;
+
+  if (!movieInDb._id) {
+    movieInDb._id = Date.now().toString();
+    movies.push(movieInDb);
+  }
+
+  return movieInDb;
 }
 
 export function deleteMovie(movieId) {
