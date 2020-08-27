475dc43c40a4f608ed1961644f68095aa5359630
Arun Kumar <kumar05.962013@gmail.com>
2020-08-27 10:29:41 +0530

    connecting with mongodb database

--- src/services/logService.js
+++ src/services/logService.js
@@ -1,16 +1,17 @@
 import * as Sentry from "@sentry/react";
 
 function init() {
-  Sentry.init({
-    dsn:
-      "https://af57468fdaee4328948f299169e50327@o439488.ingest.sentry.io/5406351",
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
-  });
+  // Sentry.init({
+  //   dsn:
+  //     "https://af57468fdaee4328948f299169e50327@o439488.ingest.sentry.io/5406351",
+  //   integrations: [new Integrations.BrowserTracing()],
+  //   tracesSampleRate: 1.0,
+  // });
 }
 
 function log(error) {
-  Sentry.captureException(error);
+  console.error(error);
+  // Sentry.captureException(error);
 }
 
 export default {
